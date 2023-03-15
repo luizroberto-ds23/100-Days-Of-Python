b3bafab90dc6a5c752c316ab632317d17b3b90d0
Luiz Roberto <luizroberto.ds23@gmail.com>
2023-03-15 00:31:24 -0300

    Criei arquivo principal Day 1
    Foi criado o arquivo principal referente ao projeto Day-1 do curso

---
+++ main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Welcome to the *Band Name* Generator!")
+#2. Ask the user for the city that they grew up in.
+id_street = input("First, tell me what is the name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+user_pet = input("now, tell me what is your first pet's name?\n")
+#4. Combine the name of their *city* and *pet* and show them their Band Name.
+print("So, your Band Name could be -> " + id_street + " " + user_pet)
